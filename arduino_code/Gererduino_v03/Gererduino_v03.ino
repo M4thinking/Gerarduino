2562a45aa784982e501ed644bb67020fa28be1f1
bbrisch <benjaminbritoschiele@gmail.com>
2022-10-05 15:55:54 -0300

    PID funcional con error en estado estacionario

--- arduino_code/Gererduino_v03/Gererduino_v03.ino
+++ arduino_code/Gererduino_v03/Gererduino_v03.ino
@@ -8,9 +8,9 @@ uint16_t sensorValues[SensorCount];
 int pin_motor_izq = 11;
 int pin_motor_der = 10;
 
-#define KD  0.01  // Constante derivativa PID
-#define KP  0.1  // Constante proporcional PID
-#define base_speed   100 // Velocidad base de los motores
+#define KD 2  // Constante derivativa PID
+#define KP 0.05  // Constante proporcional PID
+#define base_speed 100 // Velocidad base de los motores
 #define max_speed 150 // Velocidad máxima de los motores
 template <typename T>
 Print& operator<<(Print& printer, T value){
@@ -34,14 +34,17 @@ void setup()
   digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
   Serial.begin(9600);
   
-  // analogWrite(pin_motor_izq,100);
-  // analogWrite(pin_motor_der,0);
+  analogWrite(pin_motor_izq,0);
+  analogWrite(pin_motor_der,base_speed);
   Serial.println("Calibrando");
   for (uint16_t i = 0; i < 400; i++){
     qtr.calibrate();
   }
   digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
+  
+  analogWrite(pin_motor_izq,0);
+  analogWrite(pin_motor_der,0);
+  delay(1000);
   
   for (uint8_t i = 0; i < SensorCount; i++)
   {
@@ -57,7 +60,7 @@ void setup()
   }
   Serial.println();
   Serial.println();
-  delay(1000);
+  delay(200);
 }
 void loop() {
   // Generamos la referencia
@@ -68,14 +71,14 @@ void loop() {
   // the middle sensor
   int position = qtr.readLineWhite(sensorValues);
  
-  // Error will range from -450000 to +4500.  If we have sensor 0 on the left and sensor 7 on the right  
-  int error = position - 4500;
+  // Error will range from -4500 to +4500.  If we have sensor 0 on the left and sensor 7 on the right  
+  int error = (position - 4500)/100;
  
   // KP is the a floating-point proportional constant (maybe start with a value around 0.1)
   // KD is the floating-point derivative constant (maybe start with a value around 5)
   int motorSpeed = KP * error + KD * (error - lastError);
   lastError = error;
- 
+  pprint("Aditional speed: ", motorSpeed);
   // M1 and M2 = M are base motor speeds. 
   // Start with small values for M1 and M2.
   int m_izq_speed = base_speed + motorSpeed;
@@ -84,16 +87,22 @@ void loop() {
   // it might help to keep the speeds positive (this is optional)
   // note that you might want to add a similiar line to keep the speeds from exceeding
   // any maximum allowed value
-  if (m_izq_speed < 0)
+  if (m_izq_speed < 0){
+    pprint("a: ", m_izq_speed);
     m_izq_speed = 0;
-  if (m_der_speed < 0)
+  }
+  if (m_der_speed < 0){
+    pprint("b: ", m_der_speed);
     m_der_speed = 0;
+  }
     
   // Clipping de la velocidad
-   if (max_speed < m_izq_speed)
+  if (max_speed < m_izq_speed){
     m_izq_speed = max_speed;
-  if (max_speed < m_der_speed)
+  }
+  if (max_speed < m_der_speed){
     m_der_speed = max_speed;
+  }
  
   // set motor speeds using the two motor speed variables above
   analogWrite(pin_motor_izq,m_izq_speed);
@@ -110,6 +119,4 @@ void loop() {
   //  Serial.print('\t');
   //}
   //Serial.println(position);
-  
-  delay(1);
 }
