950189c44991de45fb4696145a33b1bdbcdf26de
bbrisch <benjaminbritoschiele@gmail.com>
2022-11-05 15:18:46 -0300

    Tests

---
+++ arduino_code/Test_motors/Test_motors.ino
@@ -0,0 +1,47 @@
+int pin_motor_derecho = 3;
+int pin_motor_izquierdo = 6;
+
+int s_izq_1 = 10;
+int s_izq_2 = 11;
+
+int s_der_1 = 4;
+int s_der_2 = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin_motor_derecho, OUTPUT);
+  pinMode(pin_motor_izquierdo, OUTPUT);
+
+  pinMode(s_izq_1, OUTPUT);
+  pinMode(s_izq_2, OUTPUT);
+
+  pinMode(s_der_1, OUTPUT);
+  pinMode(s_der_2, OUTPUT);
+
+  pinMode(13, OUTPUT);
+
+  digitalWrite(s_izq_1,HIGH);
+  digitalWrite(s_izq_2,LOW);
+  digitalWrite(s_der_1,HIGH);
+  digitalWrite(s_der_2,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // Inciamos el baile
+
+  digitalWrite(13,HIGH);
+  analogWrite(pin_motor_izquierdo,150);
+  delay(5000);
+  analogWrite(pin_motor_izquierdo,0);
+  
+  digitalWrite(13,LOW);
+  analogWrite(pin_motor_derecho,150);
+  delay(5000);
+  analogWrite(pin_motor_derecho,0);
+  
+  
+ 
+
+}
---
+++ arduino_code/Test_sensors/Test_sensors.ino
@@ -0,0 +1,50 @@
+#include <QTRSensors.h>
+QTRSensors qtr;
+
+int sensor_izq = 13, sensor_der = 7;
+int pos, der, izq;
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  // configure the sensors
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5,A6, A7}, SensorCount);
+  qtr.setEmitterPin(12);
+
+  // Pines de sensores infrarojo laterales en D7 y D13
+  pinMode(sensor_izq, INPUT);
+  pinMode(sensor_der, INPUT);
+
+  Serial.println("Calibrando...");
+  for (uint16_t i = 0; i < 100; i++){
+    qtr.calibrate();
+    }
+  
+  Serial.print('\n');
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pos = qtr.readLineWhite(sensorValues);
+  der = digitalRead(sensor_der);
+  izq = digitalRead(sensor_izq);
+
+  // Printeamos
+  for (uint8_t i = 0; i < SensorCount; i++){
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+
+  Serial.print(der);
+  Serial.print('\t');
+  Serial.print(izq);
+  Serial.print('\n');
+  delay(2000);
+}
